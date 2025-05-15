8b48191f001d1c24d3e67e90da99f683ac7d3fac
AhmadNawfalRamadhan <Ahmadnawfalramadhan@gmail.com>
2025-05-15 10:50:33 +0700

    Doublelinkedlist8

--- duoblelinkedlist.cpp
+++ duoblelinkedlist.cpp
@@ -33,7 +33,14 @@ void addNode()(
         if(START !NULL && nim == START->noMhs)
         {
             cout << "\nDuplicate number not allowed" << endl;
+            return;
         }
+
+        newNode->next = START;
+
+        if (START != NULL)
+        START->prev = newNode;
     }
 )
-}
+};
+//
