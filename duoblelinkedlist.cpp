6d75dfedf741498703ef94342b84007c1422760d
AhmadNawfalRamadhan <Ahmadnawfalramadhan@gmail.com>
2025-05-15 11:05:34 +0700

    Doublelinkedlist15

--- duoblelinkedlist.cpp
+++ duoblelinkedlist.cpp
@@ -63,7 +63,20 @@ void addNode()(
     newNode->prev = current;
 
     if (current->next !=NULL)
-        
+
 )
 };
 //
+
+void hapus()
+{
+    if (START == NULL)
+    {
+        cout << "\nList is empty" << endl;
+        return;
+    }
+
+    cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+    int rollNo;
+    cin >> rollNo;
+}
